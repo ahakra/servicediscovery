f54e105d843c0fd94cf7c1fe46cce7d2bccd8ec0
Ahmd Ak <ahakra@outlook.com>
2023-10-02 22:41:51 +0300

    commenting example

--- FMSService/main.go
+++ FMSService/main.go
@@ -1,12 +1,8 @@
 package main
 
 //import dynamicbuilder "github.com/ahakra/servicediscovery/FMSService/internal/builder/dynamic"
-import (
-	//"fmt"
 
-	partitionedbuilder "github.com/ahakra/servicediscovery/FMSService/internal/builder/partitioned"
-	"github.com/google/uuid"
-)
+//"fmt"
 
 const rootDir = "C:\\Users\\ahmad.akra\\Desktop\\goTesting\\DB"
 const rootDirLinux = "/home/amd/Desktop/ServiceDiscovery/DB"
@@ -73,15 +69,16 @@ func main() {
 	// 	Build()
 	//	fmt.Println(err)
 
-	partitionedBuilder := partitionedbuilder.NewPartitionedBuilder()
-	partitionedBuilder.SetData(testdata).
-		SetFileName(uuid.New().String() + ".txt").
-		SetRecordType("testRecord").
-		SetDateTimeFormat("2006-01-02 15:04").
-		SetUnifiedDateFormat("20060102").
-		SetSplitter(",").
-		SetRootDir(rootDirLinux).
-		SetStoreType("raw").
-		SetDateTimFieldLocation(1).Build()
+	//init example
+	// partitionedBuilder := partitionedbuilder.NewPartitionedBuilder()
+	// partitionedBuilder.SetData(testdata).
+	// 	SetFileName(uuid.New().String() + ".txt").
+	// 	SetRecordType("testRecord").
+	// 	SetDateTimeFormat("2006-01-02 15:04").
+	// 	SetUnifiedDateFormat("20060102").
+	// 	SetSplitter(",").
+	// 	SetRootDir(rootDirLinux).
+	// 	SetStoreType("raw").
+	// 	SetDateTimFieldLocation(1).Build()
 
 }
